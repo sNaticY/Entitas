ffad0877766b2bf8dcbe1a07c62da8ec7b270f68
snatic <snatic@outlook.com>
2026-04-18 20:42:17 +0200

    Replace DesperateDevs caching pool in runtime
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ src/Entitas/Caching/ObjectPool.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Entitas
+{
+    sealed class ObjectPool<T>
+    {
+        readonly Func<T> _factory;
+        readonly Action<T> _reset;
+        readonly Stack<T> _objects = new Stack<T>();
+
+        public ObjectPool(Func<T> factory, Action<T> reset = null)
+        {
+            _factory = factory;
+            _reset = reset;
+        }
+
+        public T Get() => _objects.Count == 0
+            ? _factory()
+            : _objects.Pop();
+
+        public void Push(T obj)
+        {
+            _reset?.Invoke(obj);
+            _objects.Push(obj);
+        }
+    }
+}
